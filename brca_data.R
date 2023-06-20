b3dea59a6b6285661056eb1b617a217db7b0c680
Ryan N <96593302+xpbowler@users.noreply.github.com>
2023-06-20 10:30:12 -0400

    Add files via upload

---
+++ brca_data.R
@@ -0,0 +1,116 @@
+library(TCGAbiolinks)
+library(SummarizedExperiment)
+library(dplyr)
+library(DT)
+library("DESeq2")
+library("pheatmap")
+library("pracma")
+library("graphics")
+library("caTools")
+
+#RNA-seq data from The Cancer Genome Atlas Program (TCGA). BRCA - Breast Invasive Carcinoma
+query <- GDCquery(
+  project = "TCGA-BRCA",data.category = "Sequencing Reads",data.type = "Gene Expression Quantification",
+  platform = "Illumina HiSeq", experimental.strategy = "RNA-Seq", legacy = FALSE)
+GDCdownload(query = query, method = "api", files.per.chunk = 10)
+#"01"="Primary Tumor" || "11"="Solid Tissue Normal"
+# or in dds
+#"1"="Primary Tumor" || "2"="Solid Tissue Normal"
+luad_raw_data <- GDCprepare(query = query)
+luad_raw_data@assays@data@listData[["raw_count"]] <- round(luad_raw_data@assays@data@listData[["raw_count"]])
+dds <- DESeqDataSet(brca_raw_data, design = ~ sample_type_id)
+
+#plot PCA, heatmap, and differential gene expression plot 
+vsd <- vst(dds, blind=FALSE)
+deg <- DESeq(dds, fitType="glmGamPoi", test = 'LRT', reduced = ~ 1)
+res <- results(deg)
+resultsNames(deg)
+resOrdered <- res[order(res$pvalue),]
+summary(res)
+sum(res$padj < 0.05, na.rm=TRUE)
+plotMA(res, ylim=c(-2,2))
+plotPCA(vsd, intgroup="sample_type_id")
+sampleDists <- dist(t(assay(vsd)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- paste(vsd$condition, vsd$type, sep="-")
+colnames(sampleDistMatrix) <- NULL
+pheatmap(sampleDistMatrix,clustering_distance_rows=sampleDists,clustering_distance_cols=sampleDists)
+
+#prepare numerical data 
+structured_data <- t(rbind(dds@colData@listData[["sample_type_id"]],vsd@assays@data@listData[[1]]))
+zeros_array <- matrix(0,length(dds@colData@listData[["sample_type_id"]]),3)
+mat <- cbind(structured_data,zeros_array)
+
+#use 80% of dataset as training set and 20% as test set 
+number_of_samples <- length(dds@colData@listData[["sample_type_id"]])
+set.seed(1)
+df = as.data.frame(mat)
+train <- df %>% dplyr::sample_frac(0.80)
+train_2 <- train[,-1]
+train_2 <- apply(as.matrix(train_2), 2, as.numeric)
+test  <- dplyr::anti_join(df, train)
+test_2 <- test[,-1]
+test_2 <- apply(as.matrix(test_2), 2, as.numeric)
+
+#generate train data images (80%)
+for (i in 1:nrow(train)){
+  temp_mat <- Reshape(train_2[i,],150,133)
+  mode(temp_mat) = "numeric"
+  if(train[i,"V1"]=='1'){
+    temp_string <- paste(c("t",i,".png"), collapse = "")
+  } else if(train[i,"V1"]=='2'){
+    temp_string <- paste(c("n",i,".png"), collapse = "")
+  }
+  png(filename = temp_string, width = 1380, height = 834)
+  img <- image(temp_mat)
+  dev.off()
+}
+
+#generate test data images (20%) 
+for (i in 1:nrow(test)){
+  temp_mat <- Reshape(test_2[i,],150,133)
+  mode(temp_mat) = "numeric"
+  if(test[i,"V1"]=='1'){
+    temp_string <- paste(c("t",i,".png"), collapse = "")
+  } else if(test[i,"V1"]=='2'){
+    temp_string <- paste(c("n",i,".png"), collapse = "")
+  }
+  png(filename = temp_string, width = 1380, height = 834)
+  img <- image(temp_mat)
+  dev.off()
+}
+
+
+
+
+#optional code to run 
+
+#optional analysis code to run
+resLFC <- lfcShrink(deg, coef="sample_type_id11", type="apeglm")
+plotMA(resLFC, ylim=c(-2,2))
+idx <- identify(res$baseMean, res$log2FoldChange)
+rownames(res)[idx]
+rld <- rlog(deg, blind=FALSE)
+
+#raw-data based CNN classification structured data
+gene_names <- append(vsd@rowRanges@ranges@NAMES,'ID',after=0)
+structured_... <- t(rbind(vsd@colData@listData[["sample_type_id"]],vsd@assays@data@listData[["raw_count"]]))
+write.csv(structured_...,"vsd_structured_data.csv")
+
+#... is a placeholder for the cancer type code, such as BRCA for Breast Cancer 
+#create single-case model testing csv files
+sample_10 <- t(structured_...[1,])
+write.csv(sample_1,"sample_1_t.csv")
+
+#create image-based CNN training labels
+image_names <- vector("character",0)
+for (i in 1:1208){
+  if(sample_type_id[i]=='01'){
+    k <- paste(c("t",i,".png"), collapse = "")
+  } else if(sample_type_id[i]=='11'){
+    k <- paste(c("n",i,".png"), collapse = "")
+  }
+  image_names <<- c(image_names,k)
+}
+..._train_labels <- cbind(image_names,sample_type_id)
+write.csv(..._train_labels,"..._train_labels.csv")
---
+++ coad_data.R
@@ -0,0 +1,110 @@
+library(TCGAbiolinks)
+library(SummarizedExperiment)
+library(dplyr)
+library(DT)
+library("DESeq2")
+library("pheatmap")
+library("pracma")
+library("graphics")
+library("caTools")
+
+#RNA-seq data from The Cancer Genome Atlas Program (TCGA). COAD - Colon Adenocarcinoma
+query <- GDCquery(
+  project = "TCGA-COAD",data.category = "Gene expression",data.type = "Gene expression quantification",
+  platform = "Illumina HiSeq", file.type  = "results",experimental.strategy = "RNA-Seq",
+  sample.type = c("Primary Tumor","Solid Tissue Normal"), legacy = TRUE)
+GDCdownload(query = query, method = "api", files.per.chunk = 10)
+#"01"="Primary Tumor" || "11"="Solid Tissue Normal"
+coad_raw_data <- GDCprepare(query = query)
+coad_raw_data@assays@data@listData[["raw_count"]] <- round(coad_raw_data@assays@data@listData[["raw_count"]])
+dds <- DESeqDataSet(coad_raw_data, design = ~ sample_type_id)
+
+vsd <- vst(dds, blind=FALSE)
+deg <- DESeq(dds, fitType="glmGamPoi", test = 'LRT', reduced = ~ 1)
+res <- results(deg)
+resultsNames(deg)
+resLFC <- lfcShrink(deg, coef="sample_type_id11", type="apeglm")
+resOrdered <- res[order(res$pvalue),]
+summary(res)
+sum(res$padj < 0.05, na.rm=TRUE)
+#plot PCA, differential gene expression plots
+plotMA(res, ylim=c(-2,2))
+#plotMA(resLFC, ylim=c(-2,2))
+idx <- identify(res$baseMean, res$log2FoldChange)
+rownames(res)[idx]
+plotPCA(vsd, intgroup="sample_type_id")
+#rld <- rlog(deg, blind=FALSE)
+sampleDists <- dist(t(assay(vsd)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- paste(vsd$condition, vsd$type, sep="-")
+colnames(sampleDistMatrix) <- NULL
+pheatmap(sampleDistMatrix,clustering_distance_rows=sampleDists,clustering_distance_cols=sampleDists)
+
+#prepare environment variables 
+sample_type_id <- dds@colData@listData[["sample_type_id"]]
+structured_data <- t(rbind(dds@colData@listData[["sample_type_id"]],vsd@assays@data@listData[[1]]))
+zeros_array <- matrix(0,200,3)
+mat <- cbind(structured_data,zeros_array)
+
+#use 80% of dataset as training set and 20% as test set 
+number_of_samples <- 200
+set.seed(1)
+df = as.data.frame(mat)
+train <- df %>% dplyr::sample_frac(0.70)
+train_2 <- train[,-1]
+train_2 <- apply(as.matrix(train_2), 2, as.numeric)
+test  <- dplyr::anti_join(df, train)
+test_2 <- test[,-1]
+test_2 <- apply(as.matrix(test_2), 2, as.numeric)
+
+#generate train data images (80%)
+for (i in 1:nrow(train)){
+  temp_mat <- Reshape(train_2[i,],150,133)
+  mode(temp_mat) = "numeric"
+  if(train[i,"V1"]=='01'){
+    temp_string <- paste(c("t",i,".png"), collapse = "")
+  } else if(train[i,"V1"]=='11'){
+    temp_string <- paste(c("n",i,".png"), collapse = "")
+  }
+  png(filename = temp_string, width = 1380, height = 834)
+  img <- image(temp_mat)
+  dev.off()
+}
+
+#generate test data images (20%) 
+for (i in 1:nrow(test)){
+  temp_mat <- Reshape(test_2[i,],150,133)
+  mode(temp_mat) = "numeric"
+  if(test[i,"V1"]=='01'){
+    temp_string <- paste(c("t",i,".png"), collapse = "")
+  } else if(test[i,"V1"]=='11'){
+    temp_string <- paste(c("n",i,".png"), collapse = "")
+  }
+  png(filename = temp_string, width = 1380, height = 834)
+  img <- image(temp_mat)
+  dev.off()
+}
+
+#optional code to run 
+#raw-data based CNN classification structured data
+gene_names <- append(vsd@rowRanges@ranges@NAMES,'ID',after=0)
+structured_coad <- t(rbind(vsd@colData@listData[["sample_type_id"]],vsd@assays@data@listData[["raw_count"]]))
+write.csv(structured_coad,"coad_structured_data.csv")
+
+#create single-case model testing csv files
+#Sample 
+sample_10 <- t(structured_kirc[1,])
+write.csv(sample_1,"sample_1_t.csv")
+
+#create image-based CNN training labels
+image_names <- vector("character",0)
+for (i in 1:1208){
+  if(sample_type_id[i]=='01'){
+    k <- paste(c("t",i,".png"), collapse = "")
+  } else if(sample_type_id[i]=='11'){
+    k <- paste(c("n",i,".png"), collapse = "")
+  }
+  image_names <<- c(image_names,k)
+}
+coad_train_labels <- cbind(image_names,sample_type_id)
+write.csv(coad_train_labels,"coad_train_labels.csv")

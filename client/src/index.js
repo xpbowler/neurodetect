6ae6afeb3156efdcb5b682157a5e24b89aad9320
xpbowler <rnquan30@gmail.com>
2024-01-13 19:06:34 -0500

    published to vercel

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
-
+//Published to Vercel
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter> 
